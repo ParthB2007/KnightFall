6fc1f69bbbb49b8e0de83d4f257357ef20bea46b
Parth Baldha <parthbaldha2007@gmail.com>
2024-07-08 09:05:46 +0530

    Veriosn 0.1

---
+++ WebApplication/wp_main.py
@@ -0,0 +1,12 @@
+import os
+def main():
+    wp_banner = """
+-----------------------------------------------------
+                    Web-Application
+-----------------------------------------------------
+    """
+    print(wp_banner)
+
+if __name__ == '__main__':
+    os.system("clear")
+    main()
